a249299b16e16b9eae5cde020eec621e6fdec9b7
JiuZhuuu <154327378+JiuZhuuu@users.noreply.github.com>
2025-10-09 18:07:22 +0800

    update
    port

--- web/backend/server.js
+++ web/backend/server.js
@@ -1,11 +1,27 @@
 //const express = require('express');
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
-const app = express();
+import helmet from 'helmet';
+import morgan from 'morgan';
 
-app.get('/', (req, res) => {
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+//console.log(PORT);
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors()); // Enable CORS for all routes
+app.use(helmet()); // Use Helmet to enhance API's security
+
+app.use(morgan('dev')); // Use Morgan for logging requests
+
+app.get('/test', (req, res) => {
+    console.log(res.getHeaders());
   res.send('Hello World!');
 });
 
- app.listen(3000, () => {
-   console.log('Server is running on port 3000');
+ app.listen(PORT, () => {
+   console.log(`Server is running on port ` + PORT);
  });
